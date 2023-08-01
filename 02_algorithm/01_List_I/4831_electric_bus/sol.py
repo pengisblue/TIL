7ba14a8b3d4ab0a02474c32429d113adefae159a
김해인 <khi762@naver.com>
2023-08-02 00:44:38 +0900

    bus

--- 02_algorithm/01_List_I/4831_electric_bus/sol.py
+++ 02_algorithm/01_List_I/4831_electric_bus/sol.py
@@ -7,10 +7,22 @@ for tc in range(1, T+1):
     K, N, M = map(int, input().split())  # 최대 이동 정류장, 종점, 충전기 갯수
     stop_numbers = list(map(int, input().split()))
     line = [0] * (N + 1)
-    bus_idx = 0
-    for num in stop_numbers:
+    bus_idx = 0  # 버스 위치
+    cnt_c = 0  # 충전 횟수
+
+    for num in stop_numbers:  # 충전기가 위치한 곳 i == line[i]
         line[num] += 1
 
-    while bus_idx < N:
-        for i in range(K, bus_idx, -1):
-            if
+    while bus_idx + K < N:
+        for i in range(bus_idx + K, bus_idx, -1):  # 가장 멀리갈 수 있는 거리부터 역으로 순회
+            if line[i]:  # 충전기가 있으면
+                bus_idx = i  # 버스를 이동
+                cnt_c += 1  # 충전 횟수 증가
+                print(bus_idx, cnt_c)
+                break
+            elif i == bus_idx + 1:  # for 문을 끝까지 순회하면
+                cnt_c = 0
+        if cnt_c == 0:
+            break
+
+    print(f'#{tc} {cnt_c}')
