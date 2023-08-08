544cb8775821b33dca0855f3a30d053019b4230d
김해인 <khi762@naver.com>
2023-08-08 09:03:30 +0900

    palindrome

--- 02_algorithm/06_String/4861_palindrome/sol.py
+++ 02_algorithm/06_String/4861_palindrome/sol.py
@@ -9,16 +9,14 @@ for tc in range(1, T+1):
     strings.extend(list(map(lambda x: ''.join(x), list(zip(*strings)))))
     palindrome = ''
     for string in strings:      # 리스트의 문자열을 순회하며 palindrome 확인
-        is_palindrome = True
-        for i in range(len(string)-M+1):        # M번째 문자까지 palindrome 확인할 때 반복 횟수
-            for j in range(i, M//2+i):          # 절반을 비교해서
-                if string[j] != string[M-1-j]:  # 다른 문자열이 있으면
+        for i in range(N-M+1):        # M번째 문자까지 palindrome 확인할 때 반복 횟수
+            is_palindrome = True
+            for j in range(0, M//2):          # 절반을 비교해서
+                if string[i+j] != string[M+i-1-j]:  # 다른 문자열이 있으면
                     is_palindrome = False       # False
                     break
             if is_palindrome:                   # string[i:M-1+i]가 palindrome 이면,
-                palindrome = string[i:M-1+i]
+                palindrome = string[i:M+i]
                 break
-        if is_palindrome:
-            break
 
     print(f'#{tc} {palindrome}')
