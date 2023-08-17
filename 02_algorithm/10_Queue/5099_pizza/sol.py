09aa6a20bed4041f2d05df093d27a2ce958ff291
김해인 <khi762@naver.com>
2023-08-17 14:28:26 +0900

    pizza

---
+++ 02_algorithm/10_Queue/5099_pizza/sol.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())    # 화덕 크기, 피자 개수
+    cheese = list(map(int, input().split()))    # 치즈의 양
+    pizza = 1   # 피자 번호
+    queue = deque()
+    for i in range(N):
+        # 피자 번호와 함께 첫 번째 피자부터 화덕에 넣어준다
+        queue.append([pizza, cheese.pop(0)])
+        pizza += 1
+    while cheese:   # 모든 피자를 화덕에 넣을 때 까지
+        check = queue.popleft()     # 치즈가 녹았는지 확인하는 작업
+        if check[1]//2 == 0:    # 치즈가 다 녹았다면
+            queue.append([pizza, cheese.pop(0)])    # 새로운 피자를 넣어준다
+            pizza += 1
+        else:
+            check[1] //= 2      # 치즈의 양을 반으로 줄이고
+            queue.append(check)     # 다시 화덕에 넣어준다
+    while queue:    # 화덕에 피자가 있는 동안
+        check = queue.popleft()
+        if not queue and check[1]//2 == 0:
+            # 더이상 화덕에 남은 피자가 없고 치즈가 다 녹았다면
+            print(f'#{tc}', check[0])
+        elif check[1]//2 != 0:  # 피자가 녹지 않았다면
+            check[1] //= 2
+            queue.append(check)
+        # 치즈가 다 녹았지만 마지막 피자가 아닌 것은 화덕에서 빼면 끝
