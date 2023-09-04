008cbc7bc7a8b349f141ab3e5a48b3a2097925cd
김해인 <khi762@naver.com>
2023-09-04 22:29:21 +0900

    commit

--- 03_algorithm2/04_Greedy/1244/sol.py
+++ 03_algorithm2/04_Greedy/1244/sol.py
@@ -7,7 +7,7 @@ for tc in range(1, T+1):
     numbers = list(numbers)
     change = int(change)
     N = len(numbers)
-    max_result = sorted(numbers, reverse=True)
-    print(''.join(numbers))
-    print(''.join(max_result))
-    print(f'#{tc}', change)
+    123
+    132 213 321
+    123 312 231 231 123 312 231 312 123
+    321 132 213 213 321 132
