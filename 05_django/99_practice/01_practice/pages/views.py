97e3567a51a333c188a7426034b6099f08df1772
김해인 <khi762@naver.com>
2023-09-14 12:32:00 +0900

    modi folder name

--- 05_django/99_practice/01_practice/pages/views.py
+++ 05_django/99_practice/01_practice/pages/views.py
@@ -3,19 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
-
-
-def info(request):
-    return render(request, 'pages/info.html')
-
-
-def skills(request):
-    return render(request, 'pages/skills.html')
-
-
-def projects(request):
-    return render(request, 'pages/projects.html')
-
-
-def contacts(request):
-    return render(request, 'pages/contacts.html')
