217c8b0f4f5642ca52bac9c07cc9d51aab000fba
김해인 <khi762@naver.com>
2023-09-19 12:27:01 +0900

    sol

--- 03_algorithm2/06_Backtracking/5208_electricbus2/sol.py
+++ 03_algorithm2/06_Backtracking/5208_electricbus2/sol.py
@@ -1,14 +1,24 @@
+from collections import deque
 import sys
 sys.stdin = open('input.txt')
 
 
-def backtracking():
-
-    pass
+def bfs():
+    queue = deque([1])
+    while queue:
+        stop = queue.popleft()
+        energy = M[stop]
+        for i in range(stop+energy, stop, -1):
+            if i >= N:
+                return visited[stop]
+            if not visited[i]:
+                visited[i] = visited[stop] + 1
+                queue.append(i)
 
 
 T = int(input())
 for tc in range(1, T+1):
     M = list(map(int, input().split()))
     N = M[0]
-    charge = 0
+    visited = [0] * N
+    print(f'#{tc} {bfs()}')
--- 03_algorithm2/Live_study/permutation.py
+++ 03_algorithm2/Live_study/permutation.py
@@ -1,27 +1,28 @@
-# now : 현재 조사 위치
-# r : 최대 조사 대상 수
+# 순열
+# nPr: n개의 요소 중 r개를 사용하는 경우의 수
 def perm(now, r):
     if now == r:
         print(check)
         return
     else:
         # 완전 검색
-        for i in range(len(num)):
+        for i in range(N):
             if visited[i]: continue
             visited[i] = True   # i번쨰 요소를 쓴다?
             # 실제 사용 여부는 check에 표기
-            check[now] = num[i]
+            check[now] = arr[i]
             perm(now + 1, r)
             visited[i] = False
 
 # 반복에 대한 이해
 # 완전 검색 : Brute Force
 # 재귀 함수에 대한 이해
-num = [1, 2, 3]
+arr = [1, 2, 3]
+N = len(arr)
 # 각 요소의 사용여부
 visited = [0, 0, 0]
-# visited = [0] * (len(num) + 1)
+# visited = [0] * (N + 1)
 # 실제 순열에 구성되는 요소들을 담을 리스트
-check = [0] * len(num)
+check = [0] * N
 
-perm(0, 2)
+perm(0, N)
