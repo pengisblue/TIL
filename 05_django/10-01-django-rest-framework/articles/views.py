b6acec0ba2dd34e2b3f80200d3b4643a13552bd7
김해인 <khi762@naver.com>
2023-10-18 14:32:43 +0900

    til

--- 05_django/10-01-django-rest-framework/articles/views.py
+++ 05_django/10-01-django-rest-framework/articles/views.py
@@ -14,10 +14,11 @@ def article_list(request):
     
     elif request.method == 'POST':
         serializer = ArticleSeriallizer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # is_valid(raise_exception=True) 를 할 경우 별도의 처리를 하지 않아도 됨
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
